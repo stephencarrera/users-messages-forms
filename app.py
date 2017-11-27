5149a18288319b819a4280d1fafd2b58fafb7941
Stephen Carrera <stephencarrera@users.noreply.github.com>
2017-11-26 22:00:12 -0800

    secret key now in dev and production

--- app.py
+++ app.py
@@ -5,16 +5,19 @@ from forms import UserForm, MessageForm, DeleteForm
 import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+	# be sure to add a key variable in $VIRTUAL_ENV/bin/postactivate
+
 
 if os.environ.get('ENV') == 'production':
 	app.config['DEBUG'] == False
 	app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL')
+
 else:
 	app.config['DEBUG'] == True
 	app.config["SQLALCHEMY_DATABASE_URI"] = 'postgres://localhost/users-messages'
 	app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-	app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-	# be sure to add a key variable in $VIRTUAL_ENV/bin/postactivate
+
 
 modus = Modus(app)
 db=SQLAlchemy(app)
